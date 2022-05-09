5663dbdc9872d88b16aa1fc9d681ae8e9d2bd44e
KamilKhametov <khametovkamil93@gmail.com>
2022-05-09 03:53:53 +0300

    room version 1 -> 2

--- app/src/main/java/com/example/hammersystems/library/coreimpl/database/ProductsDatabase.kt
+++ app/src/main/java/com/example/hammersystems/library/coreimpl/database/ProductsDatabase.kt
@@ -7,7 +7,7 @@ import com.example.hammersystems.feature.menu.domain.model.ProductEntity
 
 @Database(
     entities = [ProductEntity::class, CategoryEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class ProductsDatabase : RoomDatabase(){
